22ce8803ebd4845f45007ec745ccafa190d84c7c
Орехов Алексей Игоревич <orehovai@npfets.ru>
2018-04-25 17:24:22 +0300

    Initial commit

---
+++ app/src/main/java/ru/orehovai/pilki_foto/Main.java
@@ -0,0 +1,13 @@
+package ru.orehovai.pilki_foto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
